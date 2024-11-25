cc11f447211bc441f62c24635a7f49f05a983cb4
Scott Parlane <scott@parlanenz.com>
2024-11-25 22:30:49 +1300

    Parse the participants config

--- letsgo.py
+++ letsgo.py
@@ -4,9 +4,11 @@ Start an IMT Challenge based on a config file
 """
 
 import argparse
+import json
 import time
 
 import docker
+import yaml
 
 from services.smm import SMMServer
 
@@ -16,7 +18,25 @@ class Participant:
     State for a participant
     """
     def __init__(self, filename: str) -> None:
-        self.name = filename
+        self.load_config(filename)
+
+    def load_config(self, filename: str) -> bool:
+        """
+        Load in the participant config
+        """
+        config = None
+        if filename.endswith('.yml') or filename.endswith('.yaml'):
+            with open(filename, 'r', encoding='utf-8') as file:
+                config = yaml.safe_load(file)
+        elif filename.endswith('.json'):
+            with open(filename, 'r', encoding='utf-8') as file:
+                config = json.load(file)
+        if config is None:
+            return False
+        # These inputs should get checked
+        self.name = config['name']
+        self.members = config['members']
+        return True
 
     def start(self, docker_client) -> None:
         """
@@ -24,6 +44,16 @@ class Participant:
         """
         self.smm = SMMServer(f'{self.name}-smm', None, docker_client)
         self.smm.start()
+
+    def setup(self) -> None:
+        """
+        Setup the participant(s) accounts in this instance
+        """
+        smm_admin = self.smm.get_web_connection()
+        imt_org = smm_admin.create_organization('IMT')
+        for member in self.members:
+            user = smm_admin.create_user(member['username'], member['password'])
+            imt_org.add_member(user)
 
     def stop(self) -> None:
         """
@@ -57,6 +87,10 @@ if __name__ == "__main__":
     # Start all the services
     for participant in participant_services:
         participant.start(docker_client)
+
+    # Setup the participants in their server
+    for participant in participant_services:
+        participant.setup()
 
     # Run the IMT Challenge
     time.sleep(120)
